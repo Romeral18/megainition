2981771f7a24fa428378c5650c8559ac1ccd3f20
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-11 12:28:07 +0200

    document.get/query

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ console.log(666);
 console.log('hello' + ' ' + 'world');
 console.log('hello ' + 'world');
 console.info('Hello');
+
+document.getElementById('one').innerHTML = 1111
+
+document.querySelector('h1').innerHTML = 'rambo'
+document.querySelector('.title').innerHTML = 'HELLO'
+document.querySelector('#one').innerHTML = 'JavaScript'
+
