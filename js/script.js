eedade6d099230ccdb1c5e2c5e4980a58ff3e521
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-11 12:19:59 +0200

    logJS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log('Hello');
+console.log("6666");
+console.log('world');
+console.log(666);
+
+console.log('hello' + ' ' + 'world');
+console.log('hello ' + 'world');
+console.info('Hello');
