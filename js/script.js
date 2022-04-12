1e5f811c5cc7f8e5a220aa614e56a2c6ed4b9791
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-12 13:31:10 +0200

    Worked

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let inputIn = document.querySelector('.input-in');
+let button = document.querySelector('button')
+
+button.onclick = function () {
+    console.log('Работает!!!');
+    console.log(inputIn.value);
+}
