ab94ee64f14bf0dc18cb55883e12ca3eee021e2c
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-12 15:04:24 +0200

    Continuelater

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 const age = document.querySelector('.age');
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 
-button.onclick() => {
-    const a = 
-}
+button.onclick
