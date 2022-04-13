5badedd690e4e5e43fd31747137f1ee7358f4edf
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-13 13:18:19 +0200

    yeeeeesItsworking

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let a = 65;
+let a = 26;
 
 if (a < 25) {
     console.log('you can not enter');
@@ -8,6 +8,21 @@ else if (a >= 25 && a < 60) {
 }
 else {
     console.log('you are very old');
+}
+
+switch (a) {
+    case 15:
+        console.log('you are very young to go');
+        break;
+    case 24:
+        console.log('you are near the access');
+        break;
+    case 25:
+        console.log('great!!!!!');
+        break;
+    default:
+        console.log('okkkkkk!');
+        break;
 }
 
 
