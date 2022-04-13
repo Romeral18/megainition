481ab6ada3ce8fddeb589bf7d7fdfd63489325eb
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-13 13:12:03 +0200

    goodMorningJS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-const age = document.querySelector('.age');
-const button = document.querySelector('button');
+let a = 65;
 
-button.onclick
+if (a < 25) {
+    console.log('you can not enter');
+}
+else if (a >= 25 && a < 60) {
+    console.log('you can enter');
+}
+else {
+    console.log('you are very old');
+}
+
+
+
+
+
+
