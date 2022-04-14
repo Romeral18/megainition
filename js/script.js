b9ccd433b373113d34077d68945d38bf273e437a
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-14 10:12:26 +0200

    Co-authored-by: Evgeny Svirin <esvirin@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,46 +1,13 @@
+const input = document.querySelector('input');
 const button = document.querySelector('button');
-const input = document.querySelector('.age');
-
-button.onclick = () => {
-    const num = +input.value;
-    if (num >= 25 && num < 60) {
-        console.log('You can vote');
-    }
-    else if (num >= 60) {
-        console.log('sorry, but you can not vote. See you in next life');
-    }
-
-    else if (num != 60) {
-        console.log('You suck!');
-    }
-
-
-    else {
-        alert('oooo, you are so young. Do not worry. See yo soon!');
-    }
-
-    switch (num) {
-        case 20:
-            console.log('Happy birthday');
-            break;
-
-        case 24:
-            console.log('ufff, you are very closed');
-            break;
-        case 25:
-            console.log('Congratulations, you can change this world');
-            break;
-        case 60:
-            console.log('you can not vote for anyone:(');
-            break;
-        default:
-            console.log('Have a good day, sir');
-            break;
-
-
-    }
-}
+const list = document.querySelector('.list');
 
 
 
-
+button.addEventListener('click', (e) => {
+    const node = document.createElement('li')
+    node.innerHTML = input.value
+    list.append(node)
+    list.prepend(node)
+    inputvalue = ''
+})
