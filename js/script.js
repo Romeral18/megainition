8a0c9032647ca28886598120752b3a5f2b0ea456
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-18 12:32:31 +0200

    rangeBMJS4

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ document.querySelector('.btn-1').onclick = () => {
     const pass = document.querySelector('.logpass');
     console.log(pass.value);
 }
+
+document.querySelector('.logpass').oninput = () => {
+    const ranger = document.querySelector('.logpass').value;
+    const spany = document.querySelector('span');
+    spany.innerHTML = ranger;
+
+}
