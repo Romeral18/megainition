93578020383dfe13139a8a6635622d2dc875b7e7
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-19 14:35:08 +0200

    getElementsByClassName/getElementsByTagName

--- js/script.js
+++ js/script.js
@@ -8,9 +8,25 @@ console.log(div);
 // div.style.background = 'red';
 
 
-for (let = i = 0; i < div.length; i++) {
+for (let i = 0; i < div.length; i++) {
     console.log(div[i]);
     div[i].style.background = 'red';
+    div[i].onclick = work;
+    div[i].style.height = '100px'
 
+
+}
+
+function work() {
+    console.log('work!!!!!');
+}
+
+const b = document.getElementsByClassName('one');
+const c = document.getElementsByTagName('div')
+console.log(b);
+console.log(c);
+
+for (let i = 0; i < b.length; i++) {
+    b[i].style.border = '3px solid black';
 
 }
