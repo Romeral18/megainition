b30734c431f132967243cd1743c8e4d42f41ebdd
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-19 14:20:35 +0200

    JS5Length[]

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 
 // for
 
-for (i = 7; i > 0; i--) {
-    console.log(i);
-}
 
+
+const div = document.querySelectorAll('.one');
+console.log(div);
+// div.style.background = 'red';
+
+
+for (let = i = 0; i < div.length; i++) {
+    console.log(div[i]);
+    div[i].style.background = 'red';
+
+
+}
