ab4707f581d3276afc7c96f9fbb5f64fdf57dac0
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-19 13:46:01 +0200

    JS5FOR

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
-document.querySelector('.btn-1').onclick = () => {
-    const pass = document.querySelector('.logpass');
-    console.log(pass.value);
-}
 
-document.querySelector('.logpass').oninput = () => {
-    const ranger = document.querySelector('.logpass').value;
-    const spany = document.querySelector('span');
-    spany.innerHTML = ranger;
+// for
 
-}
-
-document.querySelector('#btn-2').onclick = (event) => {
-    event.preventDefault();
-    const form = document.querySelector('form');
-    console.log(form.elements.about.value);
-    console.log(form.elements.sometext.value);
+for (i = 7; i > 0; i--) {
+    console.log(i);
 }
 
