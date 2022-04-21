143c76258aaf713dd483e504147cdafd6f07b515
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-21 12:09:44 +0200

    JS6FORFOR

--- js/script.js
+++ js/script.js
@@ -1,52 +1,5 @@
+const out = document.querySelector('.out')
 
-// for
-
-
-
-// const div = document.querySelectorAll('.one');
-// console.log(div);
-// div.style.background = 'red';
-
-
-// for (let i = 0; i < div.length; i++) {
-//     console.log(div[i]);
-//     div[i].style.background = 'red';
-//     div[i].onclick = work;
-
-
-
-// }
-
-// function work() {
-//     console.log('work!!!!!');
-// }
-
-// const b = document.getElementsByClassName('one');
-// const c = document.getElementsByTagName('div')
-// console.log(b);
-// console.log(c);
-
-// for (let i = 0; i < b.length; i++) {
-//     b[i].style.border = '3px solid black';
-
-// }
-
-
-document.querySelector('button').onclick = () => {
-    const r = document.querySelectorAll('input[type="radio"]');
-    console.log(r);
-    for (let i = 0; i < r.length; i++) {
-        if (r[i].checked) {
-            console.log(r[i].value);
-        }
-    }
+for (let i = 0; i < 5; i++) {
+    i.innerHTML = out;
 }
-
-let out = '';
-for (let i = 0; i < 10; i++) {
-    if (i == 6) continue;
-    out += i + ' ';
-    if (i == 6) continue;
-    // if (i == 6) break;
-}
-document.querySelector('#out').innerHTML = out;
