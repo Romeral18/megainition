191d4aef144e02f6a92d849d15d0f3618130d6a8
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-21 11:30:32 +0200

    FORSPAN

--- js/script.js
+++ js/script.js
@@ -3,30 +3,46 @@
 
 
 
-const div = document.querySelectorAll('.one');
-console.log(div);
+// const div = document.querySelectorAll('.one');
+// console.log(div);
 // div.style.background = 'red';
 
 
-for (let i = 0; i < div.length; i++) {
-    console.log(div[i]);
-    div[i].style.background = 'red';
-    div[i].onclick = work;
-    div[i].style.height = '100px'
+// for (let i = 0; i < div.length; i++) {
+//     console.log(div[i]);
+//     div[i].style.background = 'red';
+//     div[i].onclick = work;
 
 
+
+// }
+
+// function work() {
+//     console.log('work!!!!!');
+// }
+
+// const b = document.getElementsByClassName('one');
+// const c = document.getElementsByTagName('div')
+// console.log(b);
+// console.log(c);
+
+// for (let i = 0; i < b.length; i++) {
+//     b[i].style.border = '3px solid black';
+
+// }
+
+
+document.querySelector('button').onclick = () => {
+    const r = document.querySelectorAll('input[type="radio"]');
+    console.log(r);
+    for (let i = 0; i < r.length; i++) {
+        if (r[i].checked) {
+            console.log(r[i].value);
+        }
+    }
 }
 
-function work() {
-    console.log('work!!!!!');
-}
+for (let i = 0; i < 10; i++) {
 
-const b = document.getElementsByClassName('one');
-const c = document.getElementsByTagName('div')
-console.log(b);
-console.log(c);
-
-for (let i = 0; i < b.length; i++) {
-    b[i].style.border = '3px solid black';
-
+    document.getElementById('out').innerHTML = i;
 }
