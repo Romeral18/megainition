205aa25712177f0cb17614dd0cb0c6d38bf59b56
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-21 13:38:46 +0200

    multiplicationTable

--- js/script.js
+++ js/script.js
@@ -9,10 +9,17 @@ console.log(out);
 
 // }
 
-for (let i = 1; i <= 30; i++) {
+// for (let i = 1; i <= 30; i++) {
 
-    for (let k = 1; k <= i; k++) {
-        out.innerHTML += '*';
+//     for (let k = 1; k <= i; k++) {
+//         out.innerHTML += '*';
+//     }
+//     out.innerHTML += '<br>';
+// }
+
+for (let i = 1; i <= 9; i++) {
+    for (let k = 1; k <= 9; k++) {
+        out.innerHTML += `${i} * ${k} = ${k * i} <br> `
     }
-    out.innerHTML += '<br>';
+    out.innerHTML += '<hr>'
 }
