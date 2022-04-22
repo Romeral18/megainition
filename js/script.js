c9892ef4534efa11c350b984b4916143d91ad557
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-22 15:04:22 +0200

    cyclesWhile

--- js/script.js
+++ js/script.js
@@ -1,86 +1,11 @@
-let f1 = document.querySelector('.f-1'); // button
-function one() {
-    console.log('work');
-
-
+for (let i = 0; i < 6; i++) {
+    console.log(i);
 }
 
-one();
-
-// f1.onclick = one; //без круглых скобок
-
-
-console.log(1 + one());
-console.log(one());
-
-function two() {
-    console.log('work 22222');
-    return 54;
+let k = 0;
+while (k < 5) {
+    k++
+    console.log('k: ' + k);
+    if (k == 3) continue;
 
 }
-
-two();
-
-console.log(1 + two());
-
-let a = 8;
-let b = 9;
-
-
-function multi() {
-    console.log(a * b);
-    return a * b;
-}
-
-
-let c1 = multi();
-let c2 = 10 * multi();
-console.log(c1, c2);
-
-function multi2(x = 9, y = 8) {
-    return x * y;
-    // выполняться не будет/будет проигнрорировано
-
-}
-
-console.log(multi2(5, 6));
-console.log(multi2(33, 23));
-console.log(multi2(a, 6));
-
-console.log(multi2());
-console.log(multi2(10));
-
-// function (){}
-f1.onclick = function () {
-    console.log('Hello');
-}
-
-f1.onclick = () => {
-    console.log('=>>>>>>');
-}
-
-f2 = document.querySelector('.f-2').onclick = function () {
-    console.log('f22222222');
-}
-
-f2 = document.querySelector('.f-2').onclick = () => {
-    console.log('arrow');
-}
-const button = document.querySelector('.f-1');
-const out1 = document.querySelector('.out-1')
-
-
-button.onclick = function t1() {
-    out1.innerHTML = a1;
-    // }
-    let a2 = 20;
-    button.onclick = function t2() {
-        console.log('hi');
-        return a2;
-    }
-
-
-    const func = function (a, b) {
-        return a * b;
-
-    }
