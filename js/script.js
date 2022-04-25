4e5866245ac882f8f9faa47d7e697b9fbd811c3d
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-25 12:51:47 +0200

    while

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@ for (let i = 0; i < 6; i++) {
 }
 
 let k = 0;
+
 while (k < 5) {
+
     k++
     console.log('k: ' + k);
-    if (k == 3) continue;
+
 
 }
+
+let sum = 0;
+let p = 0;
+while (p <= 3) {
+    sum = sum + p;
+    p++
+
+}
+console.log('sum= ' + sum);
