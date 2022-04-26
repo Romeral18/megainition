44189c09200ba67655446dd2cec4c74fd392bfd8
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-26 12:46:28 +0200

    DOM

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const one = document.querySelector('.one');
 const b1 = document.querySelector('.b-1');
 
 
-
+one.classList.add('one-2', 'one-3');
 
 const f1 = function f1() {
     one.style.width = '200px';
@@ -26,3 +26,10 @@ b2.onclick = f2;
 
 const one3 = document.querySelector('.one-3');
 const b3 = document.querySelector('.b-3');
+
+const f3 = function f3() {
+    one3.classList.remove('one-3');
+}
+
+b3.onclick = f3;
+
