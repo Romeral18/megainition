ac1cf7c14c61b278927e5783d6c5f193b917a939
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-26 12:26:15 +0200

    stylesAndButtons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-const one = document.querySelector('.one')
+const one = document.querySelector('.one');
+const b1 = document.querySelector('.b-1');
 
-one.style.width = '200px';
-one.style.height = '90px';
-one.style.backgroundColor = 'red';
-one.style.marginTop = '30px';
+
+
+
+const f1 = function f1() {
+    one.style.width = '200px';
+    one.style.height = '90px';
+    one.backgroundColor = 'red';
+    console.log('Hiiiiii');
+}
+
+b1.onclick = f1;
+
+const one2 = document.querySelector('.one-2');
+const b2 = document.querySelector('.b-2');
+
+const f2 = function f2() {
+    one2.style.backgroundColor = 'orange';
+
+
+}
+
+b2.onclick = f2;
+
+const one3 = document.querySelector('.one-3');
+const b3 = document.querySelector('.b-3');
