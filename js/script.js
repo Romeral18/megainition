b207b46c1fef7a66ed981ea09e8caebaad89b2a2
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-26 11:50:25 +0200

    whileTree

--- js/script.js
+++ js/script.js
@@ -1,22 +1,47 @@
-for (let i = 0; i < 6; i++) {
-    console.log(i);
-}
+// for (let i = 0; i < 6; i++) {
+//     console.log(i);
+// }
 
-let k = 0;
+// let k = 0;
 
-while (k < 5) {
+// while (k < 5) {
 
-    k++
-    console.log('k: ' + k);
+//     k++
+//     console.log('k: ' + k);
 
 
-}
+// }
 
-let sum = 0;
+// let sum = 0;
+// let p = 0;
+// while (p <= 3) {
+//     sum = sum + p;
+//     p++
+
+// }
+// console.log('sum= ' + sum);
+
+const out = document.querySelector('.out');
 let p = 0;
-while (p <= 3) {
-    sum = sum + p;
+let outStr = '';
+let flag = 10;
+
+
+while (p < 11) {
+    let p1 = 0;
+    while (p1 < 11) {
+        if (p1 < flag) {
+            outStr += '&nbsp';
+        }
+        else {
+            outStr += '*';
+        }
+        p1++
+    }
+    outStr += '<br>'
+    flag--
     p++
 
 }
-console.log('sum= ' + sum);
+out.innerHTML = outStr;
+console.log(out.value);
