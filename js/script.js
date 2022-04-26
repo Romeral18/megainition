a178e4d78400935373bae559a2cdf9f86c280dea
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-26 13:21:37 +0200

    DOM3

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const f1 = function f1() {
     one.style.height = '90px';
     one.backgroundColor = 'red';
     console.log('Hiiiiii');
+    console.log(document.querySelector('link').getAttribute('href'));
 }
 
 b1.onclick = f1;
@@ -18,6 +19,8 @@ const b2 = document.querySelector('.b-2');
 
 const f2 = function f2() {
     one2.style.backgroundColor = 'orange';
+    const dataInfo = cont.children[3].getAttribute('data');
+    console.log(dataInfo);
 
 
 }
@@ -32,9 +35,23 @@ const f3 = function f3() {
     cont.children[4].classList.remove('one-3');
     this.classList.add('three');
     cont.children[3].setAttribute('data', 6);
+
 }
 
 b3.onclick = f3;
 
+
+
+let a = document.createElement('div');
+
+a.innerHTML = 'Hello';
+a.classList.add('one-3');
+a.style.position = 'absolute';
+a.style.top = '500px';
+a.style.left = '200px';
+
+
+
+document.querySelector('.one').appendChild(a);
 
 
