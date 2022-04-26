d0c0950e033a5ed6f0e90983486cc0ba26bbc32c
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-26 11:59:20 +0200

    jsStyles

--- js/script.js
+++ js/script.js
@@ -1,47 +1,6 @@
-// for (let i = 0; i < 6; i++) {
-//     console.log(i);
-// }
+const one = document.querySelector('.one')
 
-// let k = 0;
-
-// while (k < 5) {
-
-//     k++
-//     console.log('k: ' + k);
-
-
-// }
-
-// let sum = 0;
-// let p = 0;
-// while (p <= 3) {
-//     sum = sum + p;
-//     p++
-
-// }
-// console.log('sum= ' + sum);
-
-const out = document.querySelector('.out');
-let p = 0;
-let outStr = '';
-let flag = 10;
-
-
-while (p < 11) {
-    let p1 = 0;
-    while (p1 < 11) {
-        if (p1 < flag) {
-            outStr += '&nbsp';
-        }
-        else {
-            outStr += '*';
-        }
-        p1++
-    }
-    outStr += '<br>'
-    flag--
-    p++
-
-}
-out.innerHTML = outStr;
-console.log(out.value);
+one.style.width = '200px';
+one.style.height = '90px';
+one.style.backgroundColor = 'red';
+one.style.marginTop = '30px';
