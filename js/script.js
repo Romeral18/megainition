117e398b36aa45d2bc9c9276ed8370996e0cc119
Vsevolod Fedoseev <vsevolod18031999@gmail.com>
2022-04-29 13:02:42 +0200

    arroyDoble

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-const a = [1, 2, 3, 4, 5];
-const b = ['z', 'x', 'y', 't'];
+const arroy = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
 
-console.log(a);
-console.log(b);
-
-a.push(6, 7, 8, 9);
-
-console.log(a);
-
-b.pop();
-
-console.log(b.pop());
-console.log(b);
-
-console.log(a);
-delete a[3];
-console.log(a);
-
-a.splice(2, 2, 'Hello MF');
-
-console.log(a);
-
-a.shift(0);
-console.log(a);
-
-a.unshift(0);
-console.log(a);
+for (let i = 0; i < arroy.length; i++) {
+    for (let k = 0; k < arroy[i].length; k++) {
+        console.log(arroy[i][k]);
+    }
+}
